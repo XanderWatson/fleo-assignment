c9860cf7a1e0dd468d9f2631b8dbb2b9d438988c
Saahil Bhavsar <saahil_bhavsar@outlook.com>
2022-05-22 10:49:50 +0530

    Removed unnecessary imports

--- backend/sales/views.py
+++ backend/sales/views.py
@@ -1,6 +1,5 @@
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from django.http import HttpResponse, HttpResponseBadRequest
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Category
 from .utilities import getColor, numOfChildren, incrementID, getNLevelChildren, categoryDetails, update
